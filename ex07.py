d55b8fc6146b1f7ce6390433e10fba4b7da1869f
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-02-13 12:48:42 +0100

    Seventh exercise

---
+++ ex07.py
@@ -0,0 +1,11 @@
+def main():
+    print("This program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    y = eval(input("Enter another number between 0 and 1: "))
+    for i in range(10):
+        x = 3.9 * (x - x * x)
+        y = 3.9 * (y - y * y)
+        print(round(x, 3), round(y, 3))
+
+
+main()
